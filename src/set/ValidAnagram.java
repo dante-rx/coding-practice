61f9a87d97b58b56b0a62033d24c122b403ab3c5
Nagendra Kumar <nagendra.a.kumar@rakuten.com>
2019-09-10 08:37:59 +0900

    few more solutions

--- src/set/ValidAnagram.java
+++ src/set/ValidAnagram.java
@@ -17,20 +17,27 @@ public class ValidAnagram {
 	 */
 	public static void main(String[] args) {
 		System.out.println(isAnagram("anagram", "nagaram"));
+		assert isAnagram("anagram", "nagaram"): true;
+		
 		System.out.println(isAnagram("aa", "bb"));
+		assert isAnagram("aa", "bb") : false;
+		
 		System.out.println(isAnagram("aba", "aab"));
+		assert isAnagram("aba", "aab"): true;
+		
 		System.out.println(isAnagram("aba", "ab"));
+		assert isAnagram("aba", "ab"): true;
 	}
 
     public static boolean isAnagram(String s, String t) {
         Map<Character, Integer> map1 = new HashMap<>();
         for (char c: s.toCharArray()){
-            map1.put( c, map1.get(c) == null ? 1: map1.get(c)+1 );
+            map1.put( c, map1.getOrDefault(c, 0)+1 );
         }
         
         Map<Character, Integer> map2 = new HashMap<>();
         for (char c: t.toCharArray()){
-            map2.put(c, map2.get(c) ==null  ? 1: map2.get(c)+1);
+            map2.put(c, map2.getOrDefault(c, 0)+1);
         }
         
         return map1.equals(map2);
