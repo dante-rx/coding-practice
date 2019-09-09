eda6deb892e6c444fe6f0806b62f4b5082fcdb48
Nagendra Kumar <nagendra.a.kumar@rakuten.com>
2019-09-10 08:46:23 +0900

    Solving few more probs

---
+++ src/codingTest/FindTotalUniqueCountries.java
@@ -0,0 +1,115 @@
+/**
+ * 
+ */
+package codingTest;
+
+import java.util.Stack;
+
+/**
+ * @author nagendra
+ *
+ */
+/**
+ * Find Total unique countries in two dimensional Array(Matrix) Countries having
+ * same code the same number. You can check only up, down, left and right
+ * 
+ * @author nagendra
+ *
+ */
+public class FindTotalUniqueCountries {
+
+	/**
+	 * @param args
+	 */
+	boolean[][] visited;
+	int[][] matrix;
+	int m;
+	int n;
+
+	public static void main(String[] args) {
+		int a1[][] = new int[][] { { 5, 4, 4 }, { 4, 3, 4 }, { 3, 2, 4 }, { 2, 2, 2 }, { 3, 3, 4 }, { 1, 4, 4 },
+				{ 4, 1, 1 } };
+		int b1[][] = new int[][] { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 },
+				{ 1, 1, 1 } };
+		FindTotalUniqueCountries ftu = new FindTotalUniqueCountries();
+		System.out.println(ftu.solution(a1)); //11
+
+		System.out.println(ftu.solution(b1));//1
+		System.out.println(ftu.solution(new int[][] { { 1, 2 } }));//2
+
+	}
+
+	public int solution(int[][] temp) {
+		matrix = temp;
+		m = matrix.length;
+		n = matrix[0].length;
+		visited = new boolean[m][n];
+		int solCount = 0;
+
+		Stack<Location> stack = new Stack<>();
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!visited[i][j]) {
+					visitNearByCountry(new Location(i, j, matrix[i][j]), stack);
+					solCount++;
+				}
+			}
+		}
+		return solCount;
+	}
+
+	private void visitNearByCountry(Location location, Stack<Location> stack) {
+		stack.add(location);
+		while (!stack.isEmpty()) {
+			Location loc = stack.pop();
+			visited[loc.x][loc.y] = true;
+			if (loc.isLeftSame(matrix, visited)) {
+				visitNearByCountry(new Location(loc.x, loc.y - 1, matrix[loc.x][loc.y - 1]), stack);
+			}
+
+			if (loc.isRightSame(n, matrix, visited)) {
+				visitNearByCountry(new Location(loc.x, loc.y + 1, matrix[loc.x][loc.y + 1]), stack);
+			}
+			if (loc.isUpSame(matrix, visited)) {
+				visitNearByCountry(new Location(loc.x - 1, loc.y, matrix[loc.x - 1][loc.y]), stack);
+			}
+
+			if (loc.isDownSame(m, matrix, visited)) {
+				visitNearByCountry(new Location(loc.x + 1, loc.y, matrix[loc.x + 1][loc.y]), stack);
+			}
+
+		}
+
+	}
+
+}
+
+class Location {
+	int x;
+	int y;
+	int value;
+
+	public Location(int x, int y, int value) {
+		this.x = x;
+		this.y = y;
+		this.value = value;
+	}
+
+	public boolean isLeftSame(int[][] map, boolean[][] visited) {
+		return this.y - 1 >= 0 && map[x][y - 1] == value && visited[x][y - 1] == false;
+	}
+
+	public boolean isRightSame(int size, int[][] map, boolean[][] visited) {
+		return this.y + 1 < size && map[x][y + 1] == value && visited[x][y + 1] == false;
+	}
+
+	public boolean isUpSame(int[][] map, boolean[][] visited) {
+		return this.x - 1 >= 0 && map[x - 1][y] == value && visited[x - 1][y] == false;
+	}
+
+	public boolean isDownSame(int size, int[][] map, boolean[][] visited) {
+		return this.x + 1 < size && map[x + 1][y] == value && visited[x + 1][y] == false;
+	}
+
+}
