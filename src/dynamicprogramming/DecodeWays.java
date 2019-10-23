5936dba0502695ed3612b2f3a229e106d5fafcf1
Nagendra Kumar <nagendra.a.kumar@rakuten.com>
2019-10-23 17:05:50 +0900

    fix

--- src/dynamicprogramming/DecodeWays.java
+++ src/dynamicprogramming/DecodeWays.java
@@ -5,9 +5,8 @@ package dynamicprogramming;
  * @author nagendra
  */
 /**
- https://leetcode.com/problems/decode-ways/
- * A message containing letters from A-Z is being encoded to numbers using the
- * following mapping:
+ * https://leetcode.com/problems/decode-ways/ A message containing letters from
+ * A-Z is being encoded to numbers using the following mapping:
  * 
  * 'A' -> 1 'B' -> 2 ... 'Z' -> 26 Given a non-empty string containing only
  * digits, determine the total number of ways to decode it.
@@ -27,35 +26,43 @@ public class DecodeWays {
 		System.out.println(decodeWays("123"));
 		System.out.println(decodeWays("121212"));
 		assert decodeWays("12") == 2;
-		assert decodeWays("123") == 3; // ABC, 
+		assert decodeWays("123") == 3; // ABC,
 		assert decodeWays("1234") == 3; // ABC,
 		System.out.println("Assertion check looks good!!");
 	}
-	
-	private static  int decodeWays(String str) {
+
+	/**
+	 * Approach check if subString(n-2, n) is a valid string or not. <br>
+	 * Check if subString(n-1, n) is a valid string or not <br>
+	 * solution[0] = 1; because a solution can be formed with empty string
+	 * 
+	 * @param str
+	 * @return
+	 */
+	private static int decodeWays(String str) {
 		int n = str.length();
-		int solution[] = new int[n+1];
+		int solution[] = new int[n + 1];
 		solution[0] = 1;
 		solution[1] = str.charAt(0) == '0' ? 0 : 1;
-		for (int i =2; i<= n ; i++){
-			String lastTwo = str.substring(i-2, i);
-			String last = str.substring(i-1, i);
-			if (isValid(last) == 1 ){
-				solution[i] += solution[i-1];
+		for (int i = 2; i <= n; i++) {
+			String lastTwo = str.substring(i - 2, i);
+			String last = str.substring(i - 1, i);
+			if (isValid(last) == 1) {
+				solution[i] += solution[i - 1];
 			}
-			if (isValid(lastTwo) == 1){
-				solution[i] += solution[i-2];
+			if (isValid(lastTwo) == 1) {
+				solution[i] += solution[i - 2];
 			}
 		}
 		return solution[n];
 	}
 
-	private static int isValid(String last){
-		if (last.length() == 1){
+	private static int isValid(String last) {
+		if (last.length() == 1) {
 			return last.charAt(0) == '0' ? 0 : 1;
-		}else if (last.length() == 2){
+		} else if (last.length() == 2) {
 			int x = Integer.parseInt(last);
-			return  x >= 10 && x <= 26 ? 1 : 0;
+			return x >= 10 && x <= 26 ? 1 : 0;
 		}
 		return 0;
 	}
